b966595798a5242933e80df26d68ef2c9a0c1db5
sruly24 <srulybrach@gmail.com>
2020-06-08 13:15:59 -0400

    added vue and such

---
+++ main.js
@@ -0,0 +1,61 @@
+var app = new Vue({
+	el: "#app",
+	data: {
+		title: "Sports Sports Sports!",
+		articleTitle: "",
+		articleBody: "",
+		cards: [
+		{
+			title: "LeBron Jimothy James",
+			snippet: "New Orleans Saints quarterback Drew Brees attracted backlash Wednesday across the sports world, including an outspoken teammate, when he reiterated his stance on how he will never agree with anybody disrespecting the flag of the United States of America during an interview with Yahoo Finance.An emotional Malcolm Jenkins, in a video that has since been deleted from social media, said he was hurt by Brees comments and that they were extremely self-centered. The Yahoo interview featured Brees first comments in the wake of George Floyds killing last week.Our communities are under siege and we need help, Jenkins said in one of the videos on Instagram. And what youre telling us is dont ask for help that way, ask for it a different way. I cant listen to it when you ask that way. Were done asking, Drew. And people who share your sentiments, who express those, and push them throughout the world, the airwaves, are the problem.And its unfortunate because I considered you a friend. I looked up to you. Youre somebody who I had a great deal of respect for. But sometimes you should shut the f--- up.Jenkins, a safety who agreed to a four-year deal with the Saints in March, later clarified his decision to take down the initial video.I recorded a few videos when thinking of how to respond to Drew Brees, I dont take any of it back-I meant what I said-I removed the 1st video because I knew it be more about the headlines, he wrote. I want people to understand how those of us struggling with whats going on feel.EDITORS PICKSBrees: Will never agree with disrespecting flagBroncos Fangio: should have been more clearJags Khan has battled racism, asks for changeJenkins, who also appeared on CNN on Wednesday, was not alone in sharing his thoughts. Brees remarks on the flag drew a sharp rebuke on social media from Green Bay Packers quarterback Aaron Rodgers and San Francisco 49ers cornerback Richard Sherman, among other NFL players, as well as from Los Angeles Lakers star LeBron James.Rodgers also posted on Instagram, seguing off a photo of him and his teammates interlocked during a national anthem, writing: A few years ago we were criticized for locking arms in solidarity before the game. It has NEVER been about an anthem or a flag. Not then. Not now. Listen with an open heart, lets educate ourselves, and then turn word and thought into action.Sherman took to Twitter on Wednesday afternoon, saying Brees was beyond lost.Guarantee you there were black men fighting along side your grandfather but this doesnt seem to be about that, Sherman wrote. That uncomfortable conversation you are trying to avoid by injecting military into a conversation about brutality and equality is part of the problem.Brees gave a lengthy response to ESPN when asked about the perceived conflict between his stances -- including a potential divide in his locker room, where players such as Jenkins and Demario Davis are among the leaders of the players coalition seeking social justice and racial equality.I love and respect my teammates, and I stand right there with them in regards to fighting for racial equality and justice, Brees said. I also stand with my grandfathers who risked their lives for this country and countless other military men and women who do it on a daily basis.Damon Harrison Sr.✔@snacksHow can you be in the locker rooms, speaking to the players, know the reasoning, and yet still be dumb enough to believe its about the flag. Like HOW???? He should know better than that. He just doesnt care. Damn man not Drew...2,2424:21 PM - Jun 3, 2020Twitter Ads info and privacy630 people are talking about thisFormer Lions defensive tackle Damon Snacks Harrison, still a free agent as the season nears, said on Twitter that a veteran of Brees caliber should have known better.How can you be in the locker rooms, speaking to the players, know the reasoning, and yet still be dumb enough to believe its about the flag, he wrote. Like HOW???? He should know better than that. He just doesnt care. Damn man not Drew ...  \n/\\n\n <h1>FROM ESPN</h1>",
+			img: "https://a.espncdn.com/combiner/i?img=%2Fphoto%2F2019%2F1111%2Fr626486_1296x729_16%2D9.jpg&w=476&h=266&scale=crop&cquality=40&location=center",
+			pageLink: "_blank"
+		},
+		{
+			title: "Tyree Irving",
+			snippet: "What sport does he play? Sruly may never know",
+			img: "https://a3.espncdn.com/combiner/i?img=%2Fphoto%2F2020%2F0305%2Fr675137_1296x729_16%2D9.jpg&w=476&h=266&scale=crop&cquality=40&location=center",
+			pageLink: "_blank"
+		},
+		{
+			title: "Tyree Irving",
+			snippet: "What sport does he play? Sruly may never know",
+			img: "https://a2.espncdn.com/combiner/i?img=%2Fphoto%2F2020%2F0601%2Fr704210_1296x729_16%2D9.jpg&w=476&h=266&scale=crop&cquality=40&location=center",
+			pageLink: "_blank"
+		},
+		{
+			title: "Tyree Irving",
+			snippet: "What sport does he play? Sruly may never know",
+			img: "https://secure.espncdn.com/combiner/i?img=/photo/2020/0601/r704358_1296x729_16-9.jpg&w=476&h=268&scale=crop&location=center&cquality=40",
+			pageLink: "_blank"
+		},
+		{
+			title: "Tyree Irving",
+			snippet: "What sport does he play? Sruly may never know",
+			img: "https://sports.cbsimg.net/images/blogs/david-tyree-07222014.jpg",
+			pageLink: "_blank"
+		}
+		],
+		slides: [
+		{
+			title: "Basketball",
+			caption: "Basketball is a sport that people play with a ball.",
+			img: "/Users/abcdef/Desktop/sports/basketball.jpg",
+			active: "active"
+
+		},
+		{
+			title: "football",
+			caption: "football is a sport that people play with a ball.",
+			img: "/Users/abcdef/Desktop/sports/football.jpg",
+			active: ""
+		},
+		{
+			title: "hockey",
+			caption: "hockey is a bad sport",
+			img: "hockey.jpg",
+			active: ""
+		}
+		]
+	}
+})
