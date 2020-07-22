c8c17beb750967f479f27794b8b50149063990cd
elikan <58127633+elikan@users.noreply.github.com>
2020-07-22 14:25:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,53 +6,61 @@ var app = new Vue({
 		articleBody: "",
 		cards: [
 		{
-			title: "LeBron Jimothy James",
+			title: "Drew Breese",
 			snippet: "New Orleans Saints quarterback Drew Brees attracted backlash Wednesday across the sports world, including an outspoken teammate, when he reiterated his stance on how he will never agree with anybody disrespecting the flag of the United States of America during an interview with Yahoo Finance.An emotional Malcolm Jenkins, in a video that has since been deleted from social media, said he was hurt by Brees comments and that they were extremely self-centered. The Yahoo interview featured Brees first comments in the wake of George Floyds killing last week.Our communities are under siege and we need help, Jenkins said in one of the videos on Instagram. And what youre telling us is dont ask for help that way, ask for it a different way. I cant listen to it when you ask that way. Were done asking, Drew. And people who share your sentiments, who express those, and push them throughout the world, the airwaves, are the problem.And its unfortunate because I considered you a friend. I looked up to you. Youre somebody who I had a great deal of respect for. But sometimes you should shut the f--- up.Jenkins, a safety who agreed to a four-year deal with the Saints in March, later clarified his decision to take down the initial video.I recorded a few videos when thinking of how to respond to Drew Brees, I dont take any of it back-I meant what I said-I removed the 1st video because I knew it be more about the headlines, he wrote. I want people to understand how those of us struggling with whats going on feel.EDITORS PICKSBrees: Will never agree with disrespecting flagBroncos Fangio: should have been more clearJags Khan has battled racism, asks for changeJenkins, who also appeared on CNN on Wednesday, was not alone in sharing his thoughts. Brees remarks on the flag drew a sharp rebuke on social media from Green Bay Packers quarterback Aaron Rodgers and San Francisco 49ers cornerback Richard Sherman, among other NFL players, as well as from Los Angeles Lakers star LeBron James.Rodgers also posted on Instagram, seguing off a photo of him and his teammates interlocked during a national anthem, writing: A few years ago we were criticized for locking arms in solidarity before the game. It has NEVER been about an anthem or a flag. Not then. Not now. Listen with an open heart, lets educate ourselves, and then turn word and thought into action.Sherman took to Twitter on Wednesday afternoon, saying Brees was beyond lost.Guarantee you there were black men fighting along side your grandfather but this doesnt seem to be about that, Sherman wrote. That uncomfortable conversation you are trying to avoid by injecting military into a conversation about brutality and equality is part of the problem.Brees gave a lengthy response to ESPN when asked about the perceived conflict between his stances -- including a potential divide in his locker room, where players such as Jenkins and Demario Davis are among the leaders of the players coalition seeking social justice and racial equality.I love and respect my teammates, and I stand right there with them in regards to fighting for racial equality and justice, Brees said. I also stand with my grandfathers who risked their lives for this country and countless other military men and women who do it on a daily basis.Damon Harrison Sr.✔@snacksHow can you be in the locker rooms, speaking to the players, know the reasoning, and yet still be dumb enough to believe its about the flag. Like HOW???? He should know better than that. He just doesnt care. Damn man not Drew...2,2424:21 PM - Jun 3, 2020Twitter Ads info and privacy630 people are talking about thisFormer Lions defensive tackle Damon Snacks Harrison, still a free agent as the season nears, said on Twitter that a veteran of Brees caliber should have known better.How can you be in the locker rooms, speaking to the players, know the reasoning, and yet still be dumb enough to believe its about the flag, he wrote. Like HOW???? He should know better than that. He just doesnt care. Damn man not Drew ...  \n/\\n\n <h1>FROM ESPN</h1>",
 			img: "https://a.espncdn.com/combiner/i?img=%2Fphoto%2F2019%2F1111%2Fr626486_1296x729_16%2D9.jpg&w=476&h=266&scale=crop&cquality=40&location=center",
 			pageLink: "_blank"
 		},
 		{
-			title: "Tyree Irving",
-			snippet: "What sport does he play? Sruly may never know",
+			title: "Zion Williamson",
+			snippet: "For years, he wasn’t human, he was just Zion—a one-name prodigy, who possessed such a magnificent talent that no one could be blamed for re-thinking Newton’s laws of gravity. In high school, we were mesmerized by his countless rim-shocking dunks that filled our Instagram feeds. We watched as Drake and Odell Beckham Jr. wore his Spartanburg Day jersey, and we observed how a singular talent made a small private school in South Carolina one of the hubs of the college basketball recruiting circuit. He joined Duke, a school with a history of hosting college basketball’s most villainous figures, and made it not only must-watch, but nearly must-love. As he stat-stuffed box scores, we learned that at one time he was a “hell of a poet,” that he enjoyed studying the history of hip hop and that he undertook a semiotic analysis of Disney films. He became the third freshman to win the Naismith National Player of the Year award. Accolades, however, don’t describe the visceral excitement of watching him play. “Zion Williamson seems like an outstanding young man as well as an outstanding basketball player,” President Obama tweeted about future No. 1 overall pick Zion Williamson in February of last year.
+",
 			img: "https://a3.espncdn.com/combiner/i?img=%2Fphoto%2F2020%2F0305%2Fr675137_1296x729_16%2D9.jpg&w=476&h=266&scale=crop&cquality=40&location=center",
 			pageLink: "_blank"
 		},
 		{
-			title: "Tyree Irving",
-			snippet: "What sport does he play? Sruly may never know",
+			title: "NFL",
+			snippet: "The league told its 32 teams Thursday that they can proceed to host training camps at the end of July, which is typical. What state bodies are saying about the NFL season carries weight. And while it’s all speculative until the pressure point of a late July opening rolls around, some of the governors with significant power over the process have made some telling comments or put forth important guidelines. With that in mind, we looked at all 23 states with teams in play and focused on what the governors have been saying about the NFL’s fall schedule or their plan to reopen their states to normal business coming out of the summer.
+
+
+",
 			img: "https://a2.espncdn.com/combiner/i?img=%2Fphoto%2F2020%2F0601%2Fr704210_1296x729_16%2D9.jpg&w=476&h=266&scale=crop&cquality=40&location=center",
 			pageLink: "_blank"
 		},
 		{
-			title: "Tyree Irving",
-			snippet: "What sport does he play? Sruly may never know",
+			title: "MLB",
+			snippet: "t’s been a long and winding road, but Opening Day is here at last. And the 2020 MLB season starts Thursday, July 23, with a bang, as the Yankees travel to the nation’s capital to face the reigning World Series-champion Nationals for a massive pitchers' duel between Gerrit Cole and Max Scherzer (7:08 p.m. ET, ESPN). Then, former American League MVP winner Mookie Betts makes his much-anticipated Dodgers debut as Los Angeles hosts its fierce rival, the Giants, in Chavez Ravine (10:08 p.m. ET, ESPN).  This will be an Opening Day like no other due to the coronavirus pandemic, obviously falling much later on the calendar and staged without standing-room-only crowds in the seats. It also kicks off a wholly unique 60-game schedule that is completely regional: Teams will play two-thirds of their games against division foes, and then the remaining one-third against clubs in geographically corresponding divisions in the opposite league (East vs. East, Central vs. Central, West vs. West). Still, there is plenty of intrigue across the Opening Day slate -- including the debuts of Anthony Rendon with the Angels (at Athletics), Josh Donaldson with the Twins (at White Sox) and Nick Castellanos and Mike Moustakas with the Reds (vs. Tigers).
+
+",
 			img: "https://secure.espncdn.com/combiner/i?img=/photo/2020/0601/r704358_1296x729_16-9.jpg&w=476&h=268&scale=crop&location=center&cquality=40",
 			pageLink: "_blank"
 		},
 		{
-			title: "Tyree Irving",
-			snippet: "What sport does he play? Sruly may never know",
+			title: "Sports history",
+			snippet: "Tyree was a special teams ace for the Giants in the mid-2000s who ended up becoming one of the most recognizable names in Giants history thanks to his amazing “helmet catch” that helped Big Blue knock off the undefeated New England Patriots, 17-14, in Super Bowl XLII. It was an unlikely play to many football fans since Tyree was not used very often as a wide receiver. Ironically, Tyree had gotten the nicked-up Plaxico Burress’ reps in practice during the week and had flubbed almost every target he got the Friday before the game. But when game-time rolled around, Tyree rose to the occasion. Whenever the Giants needed a big play on offense, Tyree was there to make it. He not only made the miraculous helmet grab, he also scored on a pass from Eli Manning early in the fourth quarter. “For me, it’s this moment that I call the monumental moment,” Tyree said of the helmet catch. “It’s always something that I can go back to and celebrate with fans and teammates. And to be a part of history is pretty monumental. It’s amazing.”
+
+",
 			img: "https://sports.cbsimg.net/images/blogs/david-tyree-07222014.jpg",
 			pageLink: "_blank"
 		}
 		],
 		slides: [
 		{
-			title: "Basketball",
-			caption: "Basketball is a sport that people play with a ball.",
+			title: "MLB",
+			caption: "Opening day is only a few days away.",
 			img: "/Users/abcdef/Desktop/sports/basketball.jpg",
 			active: "active"
 
 		},
 		{
-			title: "football",
-			caption: "football is a sport that people play with a ball.",
+			title: "NFL",
+			caption: "Will corona prevent the NFL from startnig on time?",
 			img: "/Users/abcdef/Desktop/sports/football.jpg",
 			active: ""
 		},
 		{
-			title: "hockey",
-			caption: "hockey is a bad sport",
+			title: "NHL",
+			caption: "How will the new rules after the corona virus break help or hurt the short handed playoff teams?",
 			img: "hockey.jpg",
 			active: ""
 		}
